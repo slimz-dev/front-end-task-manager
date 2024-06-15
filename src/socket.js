5bdceee9588dc288a075a62d6d0e875903025fcb
slimz <hungpj11o2@gmail.com>
2024-06-15 11:46:13 +0700

    fix socket

--- src/socket.js
+++ src/socket.js
@@ -1,3 +1,3 @@
-import io from 'socket.io-client';
+import io from 'socket.io-client/dist/socket.io';
 console.log(process.env.REACT_APP_BE_URL);
 export const socket = io(process.env.REACT_APP_BE_URL);
